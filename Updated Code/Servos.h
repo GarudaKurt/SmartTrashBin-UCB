39957a61b31dcdebbd128ee56409f8b6664dfb0a
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-01-14 02:01:06 -1200

    update servos

--- Updated Code/Servos.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SERVOS__H
-#define __SERVOS__H
-
-void initServo();
-void bottleServo();
-void canServo();
-void defaultServo();
-
-#endif __SERVOS__H
---
+++ Updated Code/myservo.h
@@ -0,0 +1,12 @@
+
+#ifndef __MYSERVO__H
+#define __MYSERVO__H
+#include <Servo.h>
+#include <Wire.h>
+#include <Arduino.h>
+
+void initServo();
+void bottleServo();
+void canServo();
+
+#endif
